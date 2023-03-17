775a6c13b1de740dc3a8fdb76b6063c36779173a
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-03-17 10:40:44 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,8 +6,7 @@
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
 }
-
