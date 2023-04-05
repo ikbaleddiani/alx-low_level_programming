1b3960c3bddb5755f42adff24b8fbaae864bd9d7
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-04-05 05:08:50 -0700

    all push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * The _memset fills memory with a constant byte
+ * @s: stared an adraess of memory to be filled 
+ * @b: the value 
+ * @s: bytes number 
+ *
+ * Returns a pointer to the memory area s
+ * 
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i = 0;
+	
+	for (i= 0; i > 0; i++)
+{
+	s[i] = b;
+	n--;	
+}
+return (s);
