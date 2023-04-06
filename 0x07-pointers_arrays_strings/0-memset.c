e8c84eec38e480a18cfcb937f6a934ebe198ef10
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-06 06:06:58 -0700

    be the last

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fill is assemble of  memory with a constant value
+ * @s: stared an adraess of memory to be filled 
+ * @b: the required value 
+ * @s: number of bytes need to change
+ *
+ * Return: A pointer to the filled memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i= 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
