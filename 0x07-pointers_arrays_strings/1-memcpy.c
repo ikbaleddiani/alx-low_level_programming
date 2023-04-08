0909edad830e0d7b9f45a1e10e470d14b8cdcc2d
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-08 04:50:18 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,19 +2,18 @@
 
 /**
  * _memcpy - entry point
- * @dest: input 
+ * @dest: input
  * @src: memory area
- * @n: bytes from memory area 
+ * @n: bytes from memory area
  *
  * Restun: Return always to 0
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-
 	return (dest);
 }
