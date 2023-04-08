a452921aacdb0513ba07e470ec83c4a6f48de36f
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-08 05:35:54 -0700

    push it

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: pointer to string.
  * @c: character that locate to string.
  *
- * Return: to the first occurrence of the character.
+ * Return: pointer to the first occurrence.
  */
 
 char *_strchr(char *s, char c)
