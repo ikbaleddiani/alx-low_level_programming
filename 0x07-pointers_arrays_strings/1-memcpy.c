90296d30203b377d57374905e03ac6c541ae698d
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-08 04:24:53 -0700

    all push

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - entry point
+ * @dest: function copies
+ * @src: memory area
+ * @n: bytes from memory area 
+ *
+ * Restun: Returns a pointer to @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest)
+}
