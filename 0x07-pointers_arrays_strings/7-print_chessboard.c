9f1eabde3f8bd159571ef60eb6cfa826e79124ab
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-10 06:07:56 -0700

    push

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	n = 0;
+
+	while (i < 8)
+
+	{
+
+		j = 0;
+
+		while (j < 8)
+
+		{
+			_putchar (a[i][j]);
+
+			j++;
+
+		}
+
+		_putchar ('\i');
+		i++;
+	}
+}
