4538f703c2429789d4796407a8e50c1938d76bb7
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-10 07:27:32 -0700

    commit it

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to a string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+{
+	putchar(*s);
+	puts_recursion(s + 1);
+}
+	else
+	putchar('\n');
+}
