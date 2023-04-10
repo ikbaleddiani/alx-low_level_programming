630ed4421ed180833ecbfac5973e3299a4260a2d
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-10 05:40:54 -0700

    commit to it all

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
  * _strspn -function that gets the length of a prexif substring
  *
  * @s: pointer to string
- * @accept: pointer to string 
+ * @accept: pointer to string
  *
  * Return: the number of bytes in the initial segment.
  */
