be70d540b6f60927e48cd75bb193272236ff377c
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-10 06:15:06 -0700

    push

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,11 +7,11 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int n, j;
 
-	i = 0;
+	n = 0;
 
-	while (i < 8)
+	while (n < 8)
 
 	{
 
@@ -20,13 +20,13 @@ void print_chessboard(char (*a)[8])
 		while (j < 8)
 
 		{
-			_putchar (a[i][j]);
+			_putchar (a[n][j]);
 
 			j++;
 
 		}
 
 		_putchar ('\n');
-		i++;
+		n++;
 	}
 }
