340d67c033383ca4c5febd4e88184b196e7e6e9e
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-11 02:54:39 -0700

    push

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,33 +7,26 @@
  * Return: the square
  */
 
-int power_operation(int n, int c)
+int square(int n, int sq)
 {
-	if (c % (n / c) == 0)
-	{
-		if (c * (n / c) == n)
-			return (c);
-		else
-			return (-1);
-	}
-	return (0 + power_operation(n, c + 1));
-
+	if (sq * sq > n)
+		return (-1);
+	else if (sq * sq == n)
+		return (sq);
+	else
+		return (square(n, sq + 1));
+}
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: int number
- *
- * Return: natural square root of 'n'
- * else -1 if 'n' does not have a natural square root
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return (power_operation(n, 2));
+	else
+		return (square(n, 0));
 }
