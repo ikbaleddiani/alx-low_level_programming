cb4c7937b635d5d0a8345a6678951374d28c5230
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-11 06:58:32 -0700

    push

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,7 +22,9 @@ char *create_array(unsigned int size, char c)
                 return (NULL);
 
         for (unsigned int i = 0; i < size; i++)
+	{
                 str[i] = c;
+	}
 
         return (str);
 }
