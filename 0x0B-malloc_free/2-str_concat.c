0b9ee67fb90ff40ef7a72e65932158eb0dabfb18
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-12 06:06:51 -0700

    push

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,34 +15,31 @@ char *str_concat(char *s1, char *s2)
 unsigned int size;
 char *ptr, ret;
 
-size = 0;
-ptr = s1;
-ptr = s2;
-if (s1 != NULL && s2 != NULL)
-    {
-        while (*ptr++)
-            size++;
-    }
-else
-    {
-        s1[0] = '\0';
-        s2[0] = '\0';
-    }
+	size = 0;
+		ptr = s1;
+	if (s1)
+		while (*ptr++)
+			size++;
+	else
+		s1 = "";
 
-ret = malloc(size + 1);
-if (ret == NULL)
-{
-return (NULL);
-}
-ptr = ret;
-{ 
-   while (*s1)
-        *ptr++ = *s1++;
-    while (*s2)
-        *ptr++ = *s2++;
-}
-    *ptr = '\0';
-{
-return ret;
-}
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size++;
+	else
+		s2 = "";
+
+	ret = malloc(size + 1);
+	if (!ret)
+		return (NULL);
+
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = 0;
+
+	return (ret);
 }
