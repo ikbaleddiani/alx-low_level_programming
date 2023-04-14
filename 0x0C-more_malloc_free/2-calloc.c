3af872f51190fb949630bf19b22dbf83e0a386c2
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-14 07:56:55 -0700

    psuh

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,19 +13,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        char *p;
-        unsigned int d;
+char *p; unsigned int d;
 
-        if (nmemb == 0 || size == 0)
-                return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-        p = malloc(nmemb * size);
-        if (p == NULL)
-                return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
 
-        size = nmemb * size;
-        for (d = 0; d < size; d++)
-                p[d] = 0;
+size = nmemb * size;
+for (d = 0; d < size; d++)
+p[d] = 0;
 
-        return (p);
+return (p);
 }	
