5c9b509113d6ed98cedd4be5f6cd387ee2bb38b4
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-14 07:21:31 -0700

    push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,48 @@
 /**
- * string_nconcat - concatenates n bytes of a string to another string
+ * string_nconcat - concatenates n bytes
+ * of a string to another string
  * @s1: 1st string
  * @s2: 2d string
  * @n: number of bytes of s2
+ *
  * Return: pointer to a newly allocated space in memory
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len_s1, len_s2;
-	char *p;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
-		;
-	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
-		;
-
-	if (n >= len_s2)
+    if (n >= strlen(s2))
+    {
+        n = strlen(s2);
+    }
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+    char *result = malloc(strlen(s1) + n + 1);
+    if (result == NULL)
+    {
+        return NULL;
+    }
+    int i = 0;
+    while (*s1)
+    {
+        result[i++] = *s1++;
+    }
+    while (n--)
+    {
+        if (*s2)
 	{
-		n = len_s2;
-		p = malloc(sizeof(char) * (len_s1 + n + 1));
-	}
-	else
-		p = malloc(sizeof(char) * (len_s1 + n + 1));
-
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < len_s1; i++)
+            result[i++] = *s2++;
+        } else
 	{
-		p[i] = s1[i];
-	}
-	for (j = 0; j < n; j++)
-	{
-		p[i++] = s2[j];
-	}
-	p[i++] = '\0';
-	return (p);
+            break;
+        }
+    }
+    result[i] = '\0';
+    return result;
 }
 
