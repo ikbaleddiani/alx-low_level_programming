568613e95eeb89b7f076e34950602e7e83dfb37d
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-14 06:47:42 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,12 +11,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+void *ptr = malloc(b);
 
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
+if (ptr == NULL)
+{
+exit(98);
+}
 
-    return ptr;
+return ptr;
 }
