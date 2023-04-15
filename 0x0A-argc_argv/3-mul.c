7c6cfdd3ecaac25b64ea8e3cf8b4bbcc74a2ff6a
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-15 16:40:22 -0700

    cd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 /**
  * main - where the main code is
- * 
  * @argc: vriable type intiger
  * @argv: variable type char
  *
- * Return: return 0 
+ * Return: return 0
  */
 
 int main(int argc, char **argv)
