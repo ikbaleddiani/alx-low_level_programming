e392160869494078a9284c60d9ee4b39b1795759
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-14 18:53:18 -0700

    dm

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,15 +4,16 @@
  * 
  * @argc: vriable type intiger
  * @argv: variable type char
+ *
  * Return: return 0 when success.
  */
 
 int main(int argc, char *argv[])
 {
-int d;
-for (d = 0; d < argc; d++)
+	int i;
+for (i = 0; i < argc; i++)
 {
-printf("%s\n", argv[d]);
+	printf("%s\n", argv[i]);
 }
-return (0);
+	return (0);
 }
