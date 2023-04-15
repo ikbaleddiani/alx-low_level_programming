4deb033ca3460e6ea99199e47fdad5b73efd8938
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-14 18:32:30 -0700

    pm

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - main function where the main code is
+ *
+ * @argc: an int variable
+ * @argv: variable type char
+ * Return: return when success
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
