2bab65b88efe87ba0f755f1b8403e63c16269747
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-14 19:04:38 -0700

    do

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,15 @@
 
 int main(int argc, char **argv)
 {
-	(void)argc;
-	if (argc == 3)
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+(void)argc;
+if (argc == 3)
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
 }
