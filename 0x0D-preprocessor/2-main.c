68625997c5c487e7211c27ceb837d06eb4f2a2b7
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-17 06:40:23 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints the name of the file
+ * it was compiled from, followed by a new line.
+ *
+ * Return: Always to 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
