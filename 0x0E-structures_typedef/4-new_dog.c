895d3f06aee09e3179106f6592a4a2e091342612
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-18 06:04:55 -0700

    s4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * *new_dog - a function that ceayes a new dog.
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ *
+ * Returns: return NULL in fails
+ */
+
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog -  a function that frees dogs.
+ * @d: pointer to a dog structure.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}	
