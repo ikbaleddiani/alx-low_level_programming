4a93e1b8d470859f1a4ea9ac43dcf05488ffbadb
eddiani ikbal <eddianiiqbal@gmail.com>
2023-04-24 03:56:30 -0700

    ineddit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,4 +10,36 @@
  * Returns: return NULL in fails
  */
 
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(d->name, name);
+
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(d->owner, owner);
+
+	d->age = age;
+
+	return (d);
+}
