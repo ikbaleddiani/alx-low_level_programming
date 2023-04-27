d222a25dac53b1ab39a873dac43b8eb807491478
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-04-27 16:11:38 -0700

    let us do this one more time

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns the number of elements in a linked list_t list
+ * list_len - a function that returns the number of elements
+ * in a linked list_t list
  * @h: pointer to the list_t
  *
  * Return: number of elements in h
