bcb52b1f7226b4f465824ee6a7bb27242a20e15b
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-04-28 05:32:17 -0700

    let us pass it and then get rest

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include "main.h"
 
+/* count_word - helper function to count the number of words in a string*/
+
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+
 /**
  * strtow - a function that splits a string into words
  * @str: string
