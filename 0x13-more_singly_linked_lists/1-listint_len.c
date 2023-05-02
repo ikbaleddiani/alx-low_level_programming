8afac55d8ae2934dc1e2e2405b378586f7aad24c
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-02 15:31:09 -0700

    prints a linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len -  a function that returns the number of elements
+ *  in a linked listint_t list.
+ *  @h: pointer
+ *
+ *  Retuen: size of list
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t d = 0;
+
+	while (h)
+	{
+		h = h->next;
+		d++;
+	}
+	return (d);
+}
