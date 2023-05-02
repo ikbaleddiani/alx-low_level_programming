76581698dad4072344570052b9d514c82931d031
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-02 16:32:22 -0700

    prints a linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,6 +11,9 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
+	
+	if (h == NULL)
+		return (0);
 
 	while (h != NULL)
 	{
@@ -18,5 +21,6 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		i++;
 	}
+	i++;
 	return (i);
 }
