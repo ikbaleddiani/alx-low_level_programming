c5dbff0520918b3969bb988ba921f85705428aff
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-02 15:26:15 -0700

    prints a linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listunt - a function that prints all the elements
+ *  of a listint_t list.
+ *  @h: ther pointer
+ *
+ *  Return: size of list
+ */
+
+size_t print_listint(const listint_t *h);
+{
+        size_t i = 0;
+
+        while (h)
+        {
+                printf("%d\n", h->);
+                h = h->next;
+                i++;
+        }
+        return (i);
+}
