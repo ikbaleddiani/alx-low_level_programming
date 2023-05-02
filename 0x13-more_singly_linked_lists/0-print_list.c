bd31e00e7a3f9d2a8cd1af8a8010277172a38f90
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-02 15:46:33 -0700

    prints a linked lists

--- 0x13-more_singly_linked_lists/0-print_list.c
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h);
 {
         size_t i = 0;
 
-        while (h)
+        while (h != NULL)
         {
-                printf("%d\n", h->);
+                printf("%d\n", h->,n);
                 h = h->next;
                 i++;
         }
