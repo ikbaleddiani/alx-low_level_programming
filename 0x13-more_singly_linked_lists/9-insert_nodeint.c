1031228ad5be2cce27088a3afe7802081c8cc6a7
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-03 16:59:59 -0700

    function that inserts an element of a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  * @head: the pointer
  * @n: data of type integer
  *
- * Return: the address of the new node, or NULL if it failed
+ * Return: the address of the new node, "NULL" if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
