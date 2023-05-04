0cdd25c4b137781efc8863efebababc42c59e07d
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-04 16:52:46 -0700

    set a bit at the index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
