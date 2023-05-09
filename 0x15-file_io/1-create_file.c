726ed1dde08fe163730d5af3430a1154860b9f95
eddiani ikbal <eddianiiqbal@gmail.com>
2023-05-09 10:31:00 -0700

    function that creat a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,32 +9,32 @@
  * @filename: named of the file
  * @text_content: texte to write
  *
- * Retuen: to the created file
+ * Return: to the created file
  */
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t text_l = 0;
+	int fd;
+	ssize_t text_l = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[text_l] != '\0')
-            text_l++;
-        if (write(fd, text_content, text_l) != text_l)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[text_l] != '\0')
+			text_l++;
+		if (write(fd, text_content, text_l) != text_l)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
