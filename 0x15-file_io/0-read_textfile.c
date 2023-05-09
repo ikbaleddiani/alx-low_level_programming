0db5d57a73fa35a62a88b6b3538624f3cf938a2c
eddiani ikbal <eddianiiqbal@gmail.com>
2023-05-09 05:46:17 -0700

    read and print function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,6 +9,9 @@
  * @letters: num of bytes
  *
  * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
