007062ac1c5cb4f83a59824031451545816655e1
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-05-12 16:57:13 -0700

    correction

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
