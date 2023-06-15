e940484364115ca6ca69f18d606e69cfd3921a91
EDDIANI IKBAL <eddianiiqbal@gmail.com>
2023-06-15 16:43:11 -0700

    add node to the head of the link

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t *));
+	if (new == NULL)
+	{
+		return NULL;
+	}
+
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
