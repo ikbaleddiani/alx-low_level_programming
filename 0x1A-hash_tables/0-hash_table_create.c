660b87d440694e23ada24df24fd5a9008d703005
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-07 16:05:08 -0700

    fixing the buges

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * hash_table_create - creates a hash table
  * @size: size of the array
  *
- * Return: pointer to the newly creates hash table
+ * Return: pointer to the newly creates hash table otherwise NULL
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
