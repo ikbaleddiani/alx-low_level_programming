f7f7d3baeaea00210b340e6297ad8ca32c77df47
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-07 17:23:45 -0700

    function that adds an element to the hash table.

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element new
+ * @ht: the  hash table
+ * @key: the key of new node
+ * @value: the value of new node
+ *
+ * Return: 1 else 0 if it fails
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	hash_node_t **table;
+
+	if (ht == NULL || value == NULL)
+		return (0);
+
+	i = key_index((unsigned char *)key, ht->size);
+	table = ht->array;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = (char *)key;
+	node->value = (char *)value;
+
+	if (table[i] != NULL)
+	{
+		node->next = table[i];
+	}
+
+	table[i] = node;
+	return (1);
+}
